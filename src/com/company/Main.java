036f11e94ded995fc0cf10a69678fd596184b5ee
Nightingale <kevin.solovjov@gmail.com>
2017-10-25 02:42:03 +0200

    Bup fixes in Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,39 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter the first tip in the format x,y");
+        int firstTipX1;
+        int firstTipY1;
+        int firstTipX2;
+        int firstTipY2;
+        int firstTipX3;
+        int firstTipY3;
+
+        int secondTipX1;
+        int secondTipY1;
+        int secondTipX2;
+        int secondTipY2;
+        int secondTipX3;
+        int secondTipY3;
+
+
+        System.out.println("Enter the x coordinate for the first tip");
         Scanner scanner = new Scanner(System.in);
-        String username = scanner.nextLine();
+        String readLine = scanner.nextLine();
+
+
+        while(true) {
+            try {
+                firstTipX1 = Integer.parseInt(readLine);
+                break;
+            } catch(NumberFormatException e) {
+                System.out.println("Wrong input, enter a number.");
+                System.out.println("Enter the x coordinate for the first tip");
+            } catch(NullPointerException e) {
+                System.out.println("Wrong input, enter a number.");
+                System.out.println("Enter the x coordinate for the first tip");
+            }
+        }
+
         System.out.println("Your username is " + username);
 
         Triunghi triangle1 = new Triunghi(new Point(1, 1), new Point(2, 2), new Point(3, 3));
